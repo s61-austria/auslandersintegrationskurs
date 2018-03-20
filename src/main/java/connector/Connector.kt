1ccc339f3d3906f5666736a53076d519f90faf13
requinard <d.diks94@gmail.com>
2018-03-20 13:40:05 +0100

    refactor(connection): extract connection details

--- src/main/java/connector/Connector.kt
+++ src/main/java/connector/Connector.kt
@@ -1,22 +1,30 @@
 package connector
 
 import com.google.gson.Gson
-import com.rabbitmq.client.*
+import com.rabbitmq.client.AMQP
+import com.rabbitmq.client.BuiltinExchangeType
+import com.rabbitmq.client.Channel
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.DefaultConsumer
+import com.rabbitmq.client.Envelope
 import model.Car
 import model.Countries
 import model.Invoice
 import model.StolenCar
-
 import java.io.IOException
 import java.time.Instant
 
-public class Connector {
+public class Connector(val username: String,
+                       val password: String,
+                       val vhost: String,
+                       val host: String) {
+
     private val factory by lazy {
         ConnectionFactory().apply {
-            username = "rabbitmq"
-            password = "rabbitmq"
-            virtualHost = "vhost"
-            host = "localhost"
+            username = this@Connector.username
+            password = this@Connector.password
+            virtualHost = this@Connector.vhost
+            host = this@Connector.host
             port = 5672
         }
     }
@@ -33,28 +41,33 @@ public class Connector {
         prepare()
     }
 
-    private fun prepare(){
+    public fun close() {
+        this.channel.close(0, "thanks!")
+    }
+
+    public fun prepare() {
         channel.exchangeDeclare(Constants.carExchangeName, BuiltinExchangeType.DIRECT)
         Countries.values().forEach {
             val queueName = "$it${Constants.carSuffix}"
-            channel.queueDeclare(queueName, true,false, false, null)
+            channel.queueDeclare(queueName, true, false, false, null)
             channel.queueBind(queueName, Constants.carExchangeName, it.toString())
         }
 
         channel.exchangeDeclare(Constants.stolenCarExchangeName, BuiltinExchangeType.DIRECT)
         Countries.values().forEach {
             val queueName = "$it${Constants.stolenCArSuffix}"
-            channel.queueDeclare(queueName, true,false, false, null)
+            channel.queueDeclare(queueName, true, false, false, null)
             channel.queueBind(queueName, Constants.stolenCarExchangeName, it.toString())
         }
 
         channel.exchangeDeclare(Constants.invoiceExchangeName, BuiltinExchangeType.DIRECT)
         Countries.values().forEach {
             val queueName = "$it${Constants.invoiceSuffix}"
-            channel.queueDeclare(queueName, true,false, false, null)
+            channel.queueDeclare(queueName, true, false, false, null)
             channel.queueBind(queueName, Constants.invoiceExchangeName, it.toString())
         }
     }
+
     private fun subscribe(queueName: String, handler: (message: String) -> Unit) {
         channel.basicConsume(queueName, false, "$queueName${Instant.now()}",
                 object : DefaultConsumer(channel) {
@@ -75,10 +88,10 @@ public class Connector {
     public fun subscribeToQueue(country: Countries, type: Any, handler: (message: String) -> Unit) {
         var queueName = "$country"
 
-        when(type) {
-            Car::class.java -> queueName += "car"
-            StolenCar::class.java -> queueName += "stolencar"
-            Invoice::class.java -> queueName += "invoice"
+        when (type) {
+            Car::class.java -> queueName += Constants.carSuffix
+            StolenCar::class.java -> queueName += Constants.stolenCArSuffix
+            Invoice::class.java -> queueName += Constants.invoiceSuffix
         }
         subscribe(queueName, handler)
     }
@@ -86,7 +99,7 @@ public class Connector {
     public fun publishCar(car: Car) {
         try {
             val serializedCar = gson.toJson(car)
-            channel.basicPublish("car", car.destinationCountry.toString(), null, serializedCar.toByteArray())
+            channel.basicPublish(Constants.carExchangeName, car.destinationCountry.toString(), null, serializedCar.toByteArray())
         } catch (e: IOException) {
             e.printStackTrace()
         }
@@ -96,7 +109,7 @@ public class Connector {
     public fun publishInvoice(invoice: Invoice) {
         try {
             val serializedInvoice = gson.toJson(invoice)
-            channel.basicPublish("invoice", invoice.destinationCountry.toString(), null, serializedInvoice.toByteArray())
+            channel.basicPublish(Constants.invoiceExchangeName, invoice.destinationCountry.toString(), null, serializedInvoice.toByteArray())
         } catch (e: IOException) {
             e.printStackTrace()
         }
@@ -106,7 +119,7 @@ public class Connector {
     fun publishStolenCar(stolenCar: Car) {
         try {
             val serializedStolenCar = gson.toJson(stolenCar)
-            channel.basicPublish("stolenCar", stolenCar.destinationCountry.toString(), null, serializedStolenCar.toByteArray())
+            channel.basicPublish(Constants.stolenCarExchangeName, stolenCar.destinationCountry.toString(), null, serializedStolenCar.toByteArray())
         } catch (e: IOException) {
             e.printStackTrace()
         }
--- src/test/java/test.kt
+++ src/test/java/test.kt
@@ -1,17 +1,28 @@
 import connector.Connector
 import model.Car
 import model.Countries
+import model.Countries.AUSTRIA
+import org.junit.After
 import org.junit.Test
 
 class test {
+    val conn by lazy {
+        Connector(
+                "rabbitmq",
+                "rabbitmq",
+                "vhost",
+                "localhost"
+        ).apply { prepare() }
+    }
+
+    @After
+    fun tearDown() {
+        conn.close()
+    }
+
     @Test
     fun testConnection() {
-        val conn = Connector()
-
-        conn.subscribeToQueue(Countries.AUSTRIA, Car::class.java, {
-            print(it)
-            assert(it != null)
-        })
+        conn.subscribeToQueue(AUSTRIA, Car::class.java, { print(it) })
 
         val car = Car(
                 "blaballba",
